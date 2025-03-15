b94c37d2357cee788587c6cc02063f2c4c0cc33e
unknown <akmalovmavlon5@gmail.com>
2025-03-15 13:16:26 +0500

    O'zgarishlar qo'shildi

---
+++ src/main/java/com/example/ecomobile/entity/ProductBrand.java
@@ -0,0 +1,17 @@
+package com.example.ecomobile.entity;
+
+import com.example.ecomobile.base.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class ProductBrand extends BaseEntity {
+    private String productBrand;
+
+}

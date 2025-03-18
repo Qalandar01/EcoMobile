1736fb81ecad3110f24010f98bdf6b35eef0fca4
Qalandar01 <qalandar2201@gmail.com>
2025-03-18 13:15:16 +0500

    corrected userrepo

--- src/main/java/com/example/ecomobile/repo/UserRepository.java
+++ src/main/java/com/example/ecomobile/repo/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.ecomobile.repo;
 
 import com.example.ecomobile.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
+    boolean existsUsersByEmail(@NotBlank(message = "Email cannot be empty") @Email(message = "Invalid email format") String email);
 }
